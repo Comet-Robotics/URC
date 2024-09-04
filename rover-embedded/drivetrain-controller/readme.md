24e7e604f4ca956668d1ebf46d8164b5cbdb7a4e
conner-replogle <95052372+conner-replogle@users.noreply.github.com>
2024-09-04 16:51:48 -0500

    Added Base Drivetrain Code

---
+++ rover-embedded/drivetrain-controller/readme.md
@@ -0,0 +1,24 @@
+# Drivetrain Controller
+The sole purpose of this controller will be to take movement commands and act on it. It will not make any decisions on when to move what to move etc. We will need to define a protocal for interacting to the controller first over serial than ethernet.
+
+
+Write now I have it incrementing our throttle then reversing and incrementing reverse very simple.
+
+
+## Project Setup
+Platform.IO IDE on VSCODE
+
+## PWM Specs
+Our range for PWM according to these specs [VictorSP](https://link.vex.com/vexpro/docs/VictorSPX-UserGuide)
+
+Frequency: 10hz -> ~350hz
+Duty Cycle:
+ - 1ms for full reverse
+ - 1.5 for 0%
+ - 2.0 for full forward
+
+
+Assuming you take the frequency 50hz
+full throttle would be 10%
+0% throttle would be 7.5%
+full reverse would be 5.0%
---
+++ rover-embedded/drivetrain-controller/src/main.cpp
@@ -0,0 +1,87 @@
+#define _PWM_LOGLEVEL_       4
+
+#include "Teensy_PWM.h"
+
+
+  #define pinToUse       5
+//creates pwm instance
+Teensy_PWM* PWM_Instance;
+
+float frequency = 500.0f;
+
+float dutyCycle = 7.5f;
+
+
+float setSpeed(float speed, bool reverse)
+{
+  if (reverse)
+  {
+    dutyCycle = 7.5f - (speed * 2.5f);
+  }
+  else
+  {
+    dutyCycle = 7.5f + (speed * 2.5f);
+  }
+
+
+  PWM_Instance->setPWM(pinToUse, frequency, dutyCycle);
+  return dutyCycle;
+}
+
+
+
+
+
+void setup()
+{
+  Serial.begin(115200);
+
+  while (!Serial && millis() < 5000);
+
+  delay(500);
+
+  Serial.print(F("\nStarting PWM_Basic using FlexTimers on "));
+
+
+  Serial.println(BOARD_NAME);
+  Serial.println(TEENSY_PWM_VERSION);
+
+  //assigns PWM frequency of 1.0 KHz and a duty cycle of 0%
+  PWM_Instance = new Teensy_PWM(pinToUse, frequency, dutyCycle);
+
+  if ( (!PWM_Instance) || !PWM_Instance->isPWMEnabled())
+  {
+    Serial.print(F("Stop here forever"));
+
+    while (true)
+      delay(10000);
+  }
+}
+
+
+
+void loop()
+{
+
+
+  delay(5000);
+  for (int i = 0; i <= 20; i++)
+  {
+    setSpeed((1.0f/20.0f)*i, false);
+    delay(500 );
+  }
+
+  PWM_Instance->setPWM(pinToUse, 0, 0);
+  delay(1000);
+
+  for (int i = 0; i <= 20; i++)
+  {
+    setSpeed((1.0f/20.0f)*i, true);
+    delay(500 );
+  }
+  PWM_Instance->setPWM(pinToUse, 0, 0);
+  delay(1000);
+
+
+  
+}
