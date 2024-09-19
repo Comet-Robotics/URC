cc131fb635d267ef74360c387b34841b506ff706
Your Name <you@example.com>
2024-09-18 19:57:36 -0600

    Added forward and turn actions

--- rover-ros/src/drivetrain/drivetrain/drivetrain_node.py
+++ rover-ros/src/drivetrain/drivetrain/drivetrain_node.py
@@ -2,27 +2,35 @@ import rclpy
 from rclpy.action import ActionServer
 from rclpy.node import Node
 import serial
-from action_drivetrain_interface.action import Drivetrain 
+from action_drivetrain_interface.action import Forward # type: ignore
+from action_drivetrain_interface.action import Turn # type: ignore
 import time
 
 class FibonacciActionServer(Node):
 
     def __init__(self):
         super().__init__('drivetrain_action_server')
-        self._action_server = ActionServer(
+        self.forward_action_server = ActionServer(
             self,
-            Drivetrain,
+            Forward,
             'forward',
-            self.execute_callback)
+            self.forward_execute_callback)
+        self.turn_action_server = ActionServer(
+            self,
+            Turn,
+            'turn',
+            self.turn_execute_callback)
         self.ser = serial.Serial(
             port='/dev/ttyACM0',  # Replace with your port
             baudrate=115200,        # Baud rate
             timeout=1             # Timeout for read operations
         )
+   
 
-    def execute_callback(self, goal_handle):
+
+    def forward_execute_callback(self, goal_handle):
         self.get_logger().info('Starting path...')
-        feedback_msg = Drivetrain.Feedback()
+        feedback_msg = Forward.Feedback()
         feedback_msg.traveled = 0
         self.ser.write(b'20:20\n')
 
@@ -35,9 +43,38 @@ class FibonacciActionServer(Node):
         self.ser.write(b'0:0\n')
         goal_handle.succeed()
 
-        result = Drivetrain.Result()
+        result = Forward.Result()
         result.total_distance = feedback_msg.traveled
         return result
+    
+
+
+    def turn_execute_callback(self, goal_handle):
+        self.get_logger().info('Starting left turn...')
+        feedback_msg = Turn.Feedback()
+        feedback_msg.degrees_moved = 0
+        if (goal_handle.request.degree > 0):
+            self.ser.write(b'-20:20\n')
+        else:
+            self.ser.write(b'20:-20\n')
+
+        try:
+
+            for i in range(1, abs(goal_handle.request.degree)):
+                feedback_msg.degrees_moved = i
+                
+                self.get_logger().info('Feedback: {0}'.format(i))
+                goal_handle.publish_feedback(feedback_msg)
+                time.sleep(0.025)
+            self.ser.write(b'0:0\n')
+            goal_handle.succeed()
+
+            result = Turn.Result()
+            result.total_degrees = feedback_msg.degrees_moved
+            return result
+        except:
+            self.ser.write(b'0:0\n')
+    
 
 
 def main(args=None):
