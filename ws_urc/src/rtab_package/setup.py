6ab1f5bab3f42fb1d122d8405d338ae86d832b96
Conner Replogle <conner.replogle@Proton.me>
2025-02-26 20:13:21 -0600

    ARM SIM

--- ws_urc/src/rtab_package/setup.py
+++ ws_urc/src/rtab_package/setup.py
@@ -11,7 +11,6 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name + '/launch', ['launch/rtab.launch.py']),
         ('share/' + package_name + '/config', ['config/ukf.yaml']),
-        ('share/' + package_name + '/config', ['config/rtabmap_config.yaml']),
         
         ('share/' + package_name, ['package.xml']),
     ],
