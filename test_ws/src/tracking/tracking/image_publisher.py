f95c5fd1d397eb9c52d00e54af1606c135672ec3
Daniel Walker <115503322+ImTitaan@users.noreply.github.com>
2025-11-10 08:34:07 -0600

    Updated image_publisher to get its video source from a parameter

--- test_ws/src/tracking/tracking/image_publisher.py
+++ test_ws/src/tracking/tracking/image_publisher.py
@@ -1,4 +1,6 @@
-# In order to get video from webcam, run this
+# vvv In order to get video from windows on WSL, ensure you have VLC and run this vvv
+#
+#  vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv  Path to your VLC installation
 # "C:\Program Files\VideoLAN\VLC\vlc.exe" dshow:// :dshow-vdev="ACER HD User Facing" :dshow-size=1280x720 :sout="#transcode{vcodec=MJPG,vb=4000,scale=1}:http{mux=mpjpeg,dst=:8080/video}" :no-sout-all :sout-keep
 #           Replace with the name of your camera/capture device ^^^^^^^^^^^^^^^^^^^
 
@@ -12,12 +14,27 @@ import numpy as np
 class ImagePublisher(Node):
     def __init__(self, Hz=10):
         super().__init__("image_publisher")
+
         self.publisher = self.create_publisher(Image, "camera/image_raw", 10)
         self.create_timer(1/Hz, self.timer_callback)
 
-        
-        self.camera = "http://10.159.188.56:8080/video" # Change to your camera
-        self.cap = cv2.VideoCapture(self.camera) # change this to the proper camera
+        # The parameter for changing the video source. Defaults to 0, the first indexed camera on the os
+        self.declare_parameter('video_source', '0')
+        video_param = self.get_parameter('video_source').get_parameter_value().string_value
+        self.get_logger().info(f"Video Source Parameter recieved: {video_param}")
+
+        try: # If the parameter is a number, we can interpret that as the camera index
+            self.video_source_id = int(video_param)
+            self.get_logger().info(f"Interpreting source as camera index: {self.video_source_id}")
+        except ValueError: # If it's not a number, it is a path or url to a video source
+            self.video_source_id = video_param
+            self.get_logger().info(f"Interpreting source as path/url: {self.video_source_id}")
+
+        self.cap = cv2.VideoCapture(self.video_source_id)
+        # Ensure the capture was opened
+        if not self.cap.isOpened():
+            self.get_logger().error("Failed to open video capture")
+
         self.cap.set(cv2.CAP_PROP_BUFFERSIZE, 1) # Set the buffer to 1 frame, so we constantly get new frames
 
         self.bridge = CvBridge()
