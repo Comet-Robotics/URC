d95654d27e110e87c8515bd2b38a19b45e5b4996
zoë_b <thezoebryant@gmail.com>
2025-11-17 19:45:46 -0600

    added headers for different site pages

--- basestation/ui/src/config/site.ts
+++ basestation/ui/src/config/site.ts
@@ -5,15 +5,27 @@ export const siteConfig = {
   description:
     "UTD URC Control Software",
   
-  /*commented out dash from navigation bar > 
-  there's only one pages why do we need that ?? - zee */
-  
-    /*mainNav: [
+    mainNav: [
     {
-      title: "Dash",
+      title: "Homepage",
+      // includes gyroscope, lidar sensor (essential), MAIN/steroscopic camera (consant feed, AI vision)
+      // battery monitoring system (remaining battery percentage & battery time remaining), gps (distance from base station & satillite position), proximity sensor (distance from objects)
+      // time > remaining and elapsed
+      // current mission > status
       href: "/",
     },
-  ],*/
+    {
+      title: "Science Payload",
+      // temp (peak & average), moisture (peak & average), science payload > chemial distribution, whats in soil
+      // ramen & vis spectometer > graph, peak, vibrational mode
+      //hyperspectral camera
+      href: "/",
+    },
+    {
+      title: "Gyroscope",// BNOSX (model, plane value) & lidar (map of surroundings), IMU (direction)
+      href: "/",
+    }
+  ],
   links: {
    
   },
