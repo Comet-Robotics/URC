aa3de80bba64730d2a954f0145961bed7d0b0788
John R <johnrami0310@gmail.com>
2026-01-14 12:33:25 -0600

    THESE NOTIFICATIONS GONNA DRIVE ME INSANE - J

--- basestation/2025_tester_ui/2025-tester-ui/app/components/header.tsx
+++ basestation/2025_tester_ui/2025-tester-ui/app/components/header.tsx
@@ -1,6 +1,15 @@
 // Header component
 
 import { Nav } from "./nav";
+import { Notifications } from "./notifications";
+
+// This function changes the display on the notification sidebar
+
+function notificationClick(){
+
+  // AUGHHHHHHHHHHH
+
+}
 
 // The function that renders the Header
 
@@ -10,6 +19,9 @@ export function Header() {
       <h1>Solis Rover Project </h1>
       <Nav />
       {/* Other stuff for the header here */}
+
+      {/* Opens Notification Sidebar */}
+      <button onClick={notificationClick}>Open Notification</button>
     </div>
   );
 }
