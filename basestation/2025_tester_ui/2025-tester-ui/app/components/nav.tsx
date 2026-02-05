9213ac1e1da5ea356024fbe798a4194830d53533
John R <johnrami0310@gmail.com>
2026-02-04 19:32:54 -0600

    I MADE THE NOTIFICATIONS WORKgit add .git add . - J

--- basestation/2025_tester_ui/2025-tester-ui/app/components/nav.tsx
+++ basestation/2025_tester_ui/2025-tester-ui/app/components/nav.tsx
@@ -5,11 +5,16 @@ import { IconButton } from '@mui/material';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import Battery90Icon from '@mui/icons-material/Battery90';
 import NetworkWifiIcon from '@mui/icons-material/NetworkWifi';
+import { Notifications } from '../components/notifications';
+import { useState } from 'react';
 // icons documentation: https://mui.com/material-ui/icons/
 
 // The function that renders the Navbar
 
 export function Nav() {
+
+  const [isVisible, setIsVisible] = useState(false);
+
   return (
     <nav>
 
@@ -19,10 +24,12 @@ export function Nav() {
         <NavLink to="/science-payload" className={"navItems"}>Science Payload</NavLink>
         <NavLink to="/gyroscope" className={"navItems"}>Gyroscope</NavLink>
         <div className="notifs">
-          <IconButton aria-label="notifs"><NotificationsIcon /></IconButton>
+          <IconButton aria-label="notifs" onClick={() => setIsVisible(!isVisible)}><NotificationsIcon /></IconButton>
           <IconButton aria-label="battery"><Battery90Icon /></IconButton>
           <IconButton aria-label="connection"><NetworkWifiIcon /></IconButton>
         </div>
+
+        {isVisible && <Notifications />}
     </nav>
   );
 }   
