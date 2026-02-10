348d83e8436c018f2e46cb767388ef61b8a1e0c1
John R <johnrami0310@gmail.com>
2026-02-09 20:12:08 -0600

    Timer front set up (working on backend) - J

--- basestation/2025_tester_ui/2025-tester-ui/app/components/footer.tsx
+++ basestation/2025_tester_ui/2025-tester-ui/app/components/footer.tsx
@@ -3,8 +3,19 @@
 export function Footer(){
     return(
         <div className = "footer">
-            <p>Current Mission: _____</p>
-            <p>Time Remaining: 00:00</p>
+            <p>Current Mission: [Insert Mission Here]</p>
+            <p>Time Remaining: 
+                <span id="hrs">00</span>
+                :
+                <span id="mins">00</span>
+                :
+                <span id="secs">00</span>
+            </p>
+            <div className="btnContain">
+                <button className="btnStart">Start</button>
+                <button className="btnPause">Stop</button>
+                <button className="btnStop">Pause</button>
+            </div>
         </div>
     )
 }
