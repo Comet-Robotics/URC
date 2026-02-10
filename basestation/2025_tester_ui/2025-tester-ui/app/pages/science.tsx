756e2c889f306c8612311a5664f7b82f46b293c0
zoë_b <thezoebryant@gmail.com>
2026-02-09 19:43:36 -0600

    addineg space for hyperspec camera on science payload page

--- basestation/2025_tester_ui/2025-tester-ui/app/pages/science.tsx
+++ basestation/2025_tester_ui/2025-tester-ui/app/pages/science.tsx
@@ -11,6 +11,11 @@ export default function Page() {
     <h1> La Princesa Rover </h1>
       <h2> Science Payload </h2>
   <p>science.tsx</p>
+
+<h3 className="video-title">Hyperspectral Camera</h3>
+  <div className = "stream-align">
+  <div className="hyperspec"></div>
+</div>
   </div>
   
   </>
