bb8a40f12b4b6f47228424a3ad0e570499ced822
John R <johnrami0310@gmail.com>
2026-02-09 19:42:08 -0600

    Footer added - J

--- basestation/2025_tester_ui/2025-tester-ui/app/pages/science.tsx
+++ basestation/2025_tester_ui/2025-tester-ui/app/pages/science.tsx
@@ -1,18 +1,20 @@
 "use client"
 
 import { Nav } from "../components/nav";
+import { Footer } from "../components/footer"
 
 export default function Page() {
   return (
   <>
-  <Nav />
-  <hr/>
-  <div className = "header">
-    <h1> La Princesa Rover </h1>
-      <h2> Science Payload </h2>
-  <p>science.tsx</p>
-  </div>
+    <Nav />
+    <hr/>
+    <div className = "header">
+      <h1> La Princesa Rover </h1>
+        <h2> Science Payload </h2>
+    <p>science.tsx</p>
+    </div>
   
+    <Footer />
   </>
 )
 }
