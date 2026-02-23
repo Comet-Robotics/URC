09b142e183abda7c3d2a03c1f33006e5c01051cb
Ana Wise <ana.wise06@gmail.com>
2026-02-23 02:06:04 -0600

    pin update pt 2

--- microcontroller_code/MotorTest/src/pins.cpp
+++ microcontroller_code/MotorTest/src/pins.cpp
@@ -16,5 +16,5 @@ ROBOT ORIENTATION
 // index starts at motor 1 to motor 6
 int MOTOR_ESC_SPEED_PINS[MOTOR_COUNT] = {21, 15, 20, 14, 17, 41}; //encoder pins (interrupt capable)
 int MOTOR_PWM_PINS[MOTOR_COUNT] = {2, 3, 4, 5, 6, 7}; // PWM capable pins
-int MOTOR_DIRECTION_PINS[MOTOR_COUNT] = {38, 37, 8, 9, 10, 11}; // Direction control pins (avoiding pin 8 conflict with LED)
+int MOTOR_DIRECTION_PINS[MOTOR_COUNT] = {38, 37, 38, 37, 38, 37}; // Direction control pins (avoiding pin 8 conflict with LED)
 int MOTOR_BRAKE_PINS[MOTOR_COUNT] = {34, 34, 34, 34, 34, 34}; // Brake control pins 
