225ff541b15ba27d63abe0f0e95c4937514ce1d7
Jason Antwi-Appah <hey@jasonaa.me>
2026-02-25 12:09:35 -0600

    feat: init steering controller project

---
+++ microcontroller_code/drivetrain_steering_node/src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include "encoder_driver.h"
+#include "pwm_driver.h"
+
+#include <micro_ros_platformio.h>
+#include <rcl/rcl.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+#include <geometry_msgs/msg/twist.h>
+#include <std_msgs/msg/float32_multi_array.h>
+
+void setup() {
+  
+}
+
+void loop() {
+  
+}
