eb2c86530bdac2a9b55f8a52f7bd425106623c08
Jason Antwi-Appah <hey@jasonaa.me>
2026-02-27 06:56:14 -0600

    ckpt

--- microcontroller_code/drivetrain_steering_node/src/main.cpp
+++ microcontroller_code/drivetrain_steering_node/src/main.cpp
@@ -9,16 +9,19 @@ Adafruit_StepperMotor *myMotor2;
 
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
 
-  delay(5000);
+  delay(5000); // delay so i can see logs :p
 
   Serial.println("Setting up Wire");
 
+  Wire.setSDA(0);  
+  Wire.setSCL(1);
   Wire.begin();
-  Wire.setClock(100000);  // 100 kHz instead of 400 kHz
+
+  Wire.setClock(100000);  // 100 kHz instead of 400 kHz (default)
   
   AFMS = Adafruit_MotorShield();
   bool success = AFMS.begin(1600, &Wire);
@@ -31,24 +34,24 @@ void setup() {
 
   myMotor = AFMS.getStepper(200, 1);
   myMotor2 = AFMS.getStepper(200, 2);
-  myMotor->setSpeed(100);
-  myMotor2->setSpeed(100);
+  myMotor->setSpeed(15);
+  myMotor2->setSpeed(15);
   Serial.println("Setup complete");
   
 }
 
 void loop() {
-  myMotor->onestep(FORWARD, DOUBLE);
-  myMotor2->onestep(FORWARD, DOUBLE);
+  myMotor->onestep(FORWARD, SINGLE);
+  // myMotor2->onestep(FORWARD, SINGLE);
   
   // Serial.println("Stepping forward");
   // digitalWrite(LED_BUILTIN, HIGH);
-  // myMotor->step(1000, FORWARD, DOUBLE);
-  // myMotor2->step(1000, FORWARD, DOUBLE);
+  // myMotor->step(1000, FORWARD, SINGLE);
+  // myMotor2->step(1000, FORWARD, SINGLE);
   // Serial.println("Stepping backward");
   // digitalWrite(LED_BUILTIN, LOW);
-  // myMotor->step(1000, BACKWARD, DOUBLE);
-  // myMotor2->step(1000, BACKWARD, DOUBLE);
+  // myMotor->step(1000, BACKWARD, SINGLE);
+  // myMotor2->step(1000, BACKWARD, SINGLE);
 
 
   // Serial.print("Step time: ");
